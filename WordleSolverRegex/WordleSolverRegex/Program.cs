c7ebf8f674ff658abf69a869e3c31ba12c2ef9e1
Fausto Ruvalcaba <se.fausto@outlook.com>
2022-02-21 10:25:58 -0800

    Add patterns, process each input

--- WordleSolverRegex/WordleSolverRegex/Program.cs
+++ WordleSolverRegex/WordleSolverRegex/Program.cs
@@ -27,42 +27,90 @@ namespace WordleSolverRegex
 
             string letters = "";
             Console.WriteLine("start with 'CRANE'");
+            string word = "CRANE";
             do
             {
                 Console.WriteLine("Type 0 for Gray, 1 for Yellow, 2 for Green (e.g. 00112)");
                 var input = Console.ReadLine();
 
-                //process results
+                //process input
+                letterPattern = ProcessInput(letterPattern, input, word, ref letters);
 
                 Regex regex = new Regex(String.Join("", letterPattern), RegexOptions.IgnoreCase);
 
-                //var matches = regex.Matches(wordList.ToString());
-                //var possibleAnswers = new List<string>();
+                var matches = regex.Matches(wordList.ToString());
+                var possibleAnswers = new List<string>();
 
-                //Console.WriteLine($"Listing matches: Count {matches.Count}");
-                //Console.WriteLine($"Must Include: {letters}");
-                //foreach (Match match in matches)
-                //{
-                //    var m = match.Value;
+                Console.WriteLine($"Listing matches: Count {matches.Count}");
+                Console.WriteLine($"Must Include: {letters}");
+                foreach (Match match in matches)
+                {
+                    var m = match.Value;
 
-                //    if (letters.ToUpper().All(value => m.ToUpper().Contains(value)))
-                //        possibleAnswers.Add(m);
-                //}
+                    if (letters.ToUpper().All(value => m.ToUpper().Contains(value)))
+                        possibleAnswers.Add(m);
+                }
 
-                //var random = new Random();
-                //var nextIndex = random.Next(possibleAnswers.Count);
+                var random = new Random();
+                var nextIndex = random.Next(possibleAnswers.Count);
+                word = possibleAnswers[nextIndex].ToUpper();
+                Console.WriteLine($"Try word: {word} ");
 
-                //Console.WriteLine($"Try word: {possibleAnswers[nextIndex]} ");
-
-                //Console.ReadLine();
+                Console.ReadLine();
 
             } while (true);
+        }
 
+        private static List<string> ProcessInput(List<string> letterPattern, string input,
+            string word, ref string letters)
+        {
 
+            for (int charCount = 0; charCount < input.Length; charCount++)
+            {
+                char currentChar = word[charCount];
 
+                if (input[charCount] == '0')
+                {
+                    //remove from all lists
+                    //remove from every other list
+                    for (int patternCount = 0; patternCount < letterPattern.Count; patternCount++)
+                    {
+                        if (IsPattern(letterPattern, patternCount) && letterPattern[patternCount].Contains(currentChar))
+                            letterPattern[patternCount] = letterPattern[patternCount].Remove(letterPattern[patternCount].IndexOf(currentChar), 1);
+                    }
+                }
+                else if (input[charCount] == '1')
+                {
+                    if (!letters.Contains(currentChar))
+                    {
+                        letters += currentChar;
+                    }
 
+                    //remove from every other list
+                    for (int patternCount = 0; patternCount < letterPattern.Count; patternCount++)
+                    {
+                        if (patternCount != charCount)
+                            continue;
 
-         
+                        letterPattern[patternCount] = letterPattern[patternCount].Remove(letterPattern[patternCount].IndexOf(currentChar), 1);
+                    }
+                }
+                else if (input[charCount] == '2')
+                {
+                    if (!letters.Contains(currentChar))
+                    {
+                        letters += currentChar;
+                    }
+
+                    letterPattern[charCount] = currentChar.ToString();
+                }
+            }
+            return letterPattern;
+        }
+
+        private static bool IsPattern(List<string> letterPattern, int patternCount)
+        {
+            return letterPattern[patternCount].Contains("[");
         }
     }
 }
